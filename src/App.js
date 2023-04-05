bf50f9411825d7f3fc2472f7f420b2c2ea062277
KyaamiRei <kolyavasilenko2703@gmail.com>
2023-04-05 16:58:23 +0300

    init redirects

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import { Route, Routes } from 'react-router-dom';
 import { useTelegram } from './hooks/useTelegram';
 
 import { Header } from './components/HeaderApp/HeaderApp';
-import { Button } from './components/Button/Button';
 import { ProductList } from './components/ProductList/ProductList';
 import { Form } from './components/Form/Form';
 
 import './App.css';
 
 function App() {
-  const { tg, onTogleButton } = useTelegram();
+  const { tg } = useTelegram();
 
   useEffect(() => {
     tg.ready();
